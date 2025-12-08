5a31bd5ee3897a2a9feb418b3f354a43cb483e1b
sagardev09 <sagardbs8121@gmail.com>
2025-12-08 17:17:03 +0530

    chore: update compatibility date and restructure wrangler.jsonc configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,22 +3,25 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'media.istockphoto.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+        pathname: "/**",
       },
     ],
   },
 };
 
 export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
